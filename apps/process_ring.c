4b5b911b0bbbbfc972574997b8556c4eb426b9e2
Helen Dougherty <ladyl3na@gmail.com>
2017-09-07 21:50:02 -0400

    assignment 2 - initial commits

---
+++ apps/process_ring.c
@@ -0,0 +1,11 @@
+/* apps/process_ring.c - helper for
+ * process_ring shell command.
+ */
+
+#include <xinu.h>
+#include <string.h>
+#include <stdio.h>
+
+process process_ring(){
+  //code
+}
--- include/process_ring.h
+++ include/process_ring.h
@@ -2,5 +2,15 @@
  * shell command.
  */
 
- #include <xinu.h>
- 
+//FUNCTION DEFINITIONS
+shellcmd xsh_process_ring(int, char*);
+process process_ring();
+
+//VARIABLE DEFINITIONS
+#define MAXP 64
+#define MINP 0
+#define DEFAULTP 2
+#define MAXR MAXINT
+#define DEFAULTR 3
+#define POLL "poll"
+#define SYNC "sync"
