5fbec9ec64bf97fa557ac36522c06a5487224369
Helen Dougherty <ladyl3na@gmail.com>
2017-09-08 17:18:33 -0400

    will it work??? no idea...

--- apps/process_ring.c
+++ apps/process_ring.c
@@ -6,14 +6,14 @@
 #include <string.h>
 #include <stdio.h>
 
-process process_ring_poll(int *pol, int ind, int len, int ival){
+process process_ring_poll(int *pol, int ind, int len, int ival, int mrnd){
   //code
   int last = ival+1;  //starter val to indicate you haven't started calc.
   int rnd = 0;
-  while(last >= 0){
+  while(last >= 0 && rnd < mrnd){
     //poll to see if there's a new value to print
     int tmp = pol[ind];
-    if(last != tmp){
+    if(last != tmp && tmp >= 0){
       printf("Ring Element %d : Round %d : Value : %d\n", ind, rnd, tmp);
       if(ind+1 == len){
         //if we're at the end of the list, update the first inbox
@@ -26,9 +26,24 @@ process process_ring_poll(int *pol, int ind, int len, int ival){
       rnd++;      //increase the round count
     }
   }
+  //if we're out here mrnds have passed and we shouldn't do more comp.
   return 0;
 }
 
-process process_ring_sync(){
+process process_ring_sync(pid32 *pids, int ind, int len, int ival, pid32 parent, int mrnds){
   //code
+  int last = val+1;
+  int round = 0;
+  while(last < 0 && round < mrnds){
+    last = receive();
+    printf("Ring Element %d : Round %d : Value : %d\n", ind, rnd, tmp);
+    if(ind+1 == len){
+      send(pids[0], last-1);
+    }else{
+      send(pids[ind+1], last-1);
+    }
+  }
+  //if we're here we're done, leave and never come back
+  send(parent, OK);
+  return 0;
 }
--- shell/xsh_process_ring.c
+++ shell/xsh_process_ring.c
@@ -107,26 +107,36 @@ xsh_process_ring(int nargs, char *args[]){
     return 1;
   }
   // start setting up and queueing up processes
+  char name[6] = "proc_";
+  char str[12];
   if(i == POLL){
     //if polling is chosen
     volatile int pol[p];
     pol[0] = val;
     for(i = 1; i < p; i++){
       pol[i] = val+1; //initialize all the other inboxes to larger number to
-                      //avoid out-of-order operations
+                      //avoid out-of-order processing
     }
-    int len = p;
-    char name[6] = "proc_";
-    char str[12];
+
     for(i = 0; i < p; i++){
       //start queueing up processes
       name[5] = sprintf(str, "%d", i);
-      resume(create(process_ring_poll, 1024, 20, name, *pol, i, p, val,r-1));
+      resume(create(process_ring_poll, 1024, 20, name, 4, pol, i, p, val, r));
     }
     //wait for polling to finish - last element needs to reach 0
-    while(pol[p-1] < 0);
+    while(pol[p] > 0);
   }else if(i == SYNC){
     //else if sync is chosen
+    pid32 pids[p];
+    pid32 parent = getpid();
+    for(i = 0; i < p; i++){
+      name[5] = sprintf(str, "%d", i);
+      pids[i] = create(process_ring_sync, 1024, 20, name, 5, pids, i, p, val, parent, r);
+      resume(pids[i]);
+    }
+    for(i = 0; i < p; i++){
+      receive(); //i have no idea if this is actually gonna work...
+    }
   }
   finish = gettime(&now);
   if (finish == SYSERR) {
