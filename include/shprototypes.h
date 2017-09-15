e49c480c134e275ffccf8aeaa7ec3c7d4dff4feb
Helen Therese Dougherty <htdoughe@indiana.edu>
2017-09-15 14:38:16 -0400

    validate_list should work?

--- include/shprototypes.h
+++ include/shprototypes.h
@@ -84,3 +84,6 @@ shellcmd xsh_hello		(int, char *[]);
 
 /* in file xsh_process_ring.c */
 shellcmd xsh_process_ring	(int, char *[]);
+
+/* in file xsh_testitem.c */
+shellcmd xsh_testitem		(int, char *[]);
