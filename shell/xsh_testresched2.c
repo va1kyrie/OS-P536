957786df472bf8c7d87bb454b2b1bf3ef6113d69
Helen Dougherty <ladyl3na@gmail.com>
2017-09-15 20:12:44 -0400

    no that move was the idiot one

--- shell/xsh_testresched2.c
+++ shell/xsh_testresched2.c
@@ -14,10 +14,10 @@ process printing(){
 /* xsh_testresched(int, char*): tests resched2
 */
 shellcmd xsh_testresched2(int nargs, char *args[]) {
- create(printing, 1024, 25, "printing-hiprio", 0);
+ resume(create(printing, 1024, 25, "printing-hiprio", 0));
  resched2(PR_READY);
  printf("parent process %d says hi!\n", getpid());
- create(printing, 1024, 20, "printing-sameprio",0);
+ resume(create(printing, 1024, 10, "printing-sameprio",0));
  resched2(PR_CURR);
  printf("parent process %d says hi! (should be second to last?)\n", getpid());
  return 0;
