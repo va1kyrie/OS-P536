2fe08d71e5b66fc55749f0c609b7c8416da5a199
Helen Dougherty <ladyl3na@gmail.com>
2017-09-22 13:37:44 -0400

    more typos

--- shell/xsh_badprio.c
+++ shell/xsh_badprio.c
@@ -31,7 +31,7 @@ shellcmd xsh_badprio(int nargs, char *args[]) {
 	pid32 parentid = getpid();
 
 	pri16 chprio = resume(create(childtest, 1024, 20, "child", 1, parentid));
-	printf("parent process: child prio is %d initially\n", childprioinit);
+	//printf("parent process: child prio is %d initially\n", childprioinit);
 	pid32 childpid = receive();
 	//printf("childpid == %d\n", childpid);
 	//pri16 chprio = resume(childpid);
