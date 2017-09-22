e69235028aabf7d54b8f4a5556f8397a1a243dd0
Helen Dougherty <ladyl3na@gmail.com>
2017-09-22 14:24:17 -0400

    ?

--- shell/xsh_badprio.c
+++ shell/xsh_badprio.c
@@ -36,8 +36,8 @@ shellcmd xsh_badprio(int nargs, char *args[]) {
 
 	pid32 parentid = getpid();
 
-	pri16 chprio = resume(create(childtest, 1024, 35, "child", 1, parentid));
-	pri16 chprio2 = resume(create(childtest, 1024, 22, "child2", 1, parentid));
+	pri16 chprio = resume(create(childtest, 1024, 22, "child", 1, parentid));
+	pri16 chprio2 = resume(create(childtest, 1024, 35, "child2", 1, parentid));
 	//printf("parent process: child prio is %d initially\n", childprioinit);
 	pid32 childpid = receive();
 	receive();
