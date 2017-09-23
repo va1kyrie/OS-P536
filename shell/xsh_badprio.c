5ae2d268166df4d66cf145c0ab006bfece01cb1e
Helen Dougherty <ladyl3na@gmail.com>
2017-09-22 20:47:48 -0400

    try this

--- shell/xsh_badprio.c
+++ shell/xsh_badprio.c
@@ -69,8 +69,8 @@ shellcmd xsh_badprio(int nargs, char *args[]) {
 	pid32 childpid = create(childtest, 1024, 35, "child", 1, parentid);
 	resched();
 	pid32 child2 = create(childtest2, 1024, 45, "child2", 1, parentid);
-	pri16 ch1 = resume2(childpid);
 	pri16 ch2 = resume2(child2);
+	pri16 ch1 = resume2(childpid);
 	resched();
 	//printf("parent process: child prio is %d initially\n", childprioinit);
 
