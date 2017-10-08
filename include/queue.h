d7c42784db1c962af16fbcb32b0dbd2c05ac0dab
Helen Dougherty <ladyl3na@gmail.com>
2017-10-07 23:31:32 -0400

    maybe this will work

--- include/queue.h
+++ include/queue.h
@@ -4,6 +4,7 @@
 
 /* Default # of queue entries: 1 per process plus 2 for ready list plus	*/
 /*			2 for sleep list plus 2 per semaphore		*/
+#define NFUT 100
 #ifndef NQENT
 #define NQENT	(NPROC + 4 + NSEM + NSEM + NFUT + NFUT)
 #endif
