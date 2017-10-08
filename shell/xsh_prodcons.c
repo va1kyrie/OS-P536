ada3a8073c378e9a6f7a64245d462006bd750574
Helen Dougherty <ladyl3na@gmail.com>
2017-10-08 00:23:25 -0400

    ???

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -33,21 +33,21 @@ resume(create(future_cons, 1024, 21, "fcons1", 1, f_exclusive));
 resume(create(future_prod, 1024, 21, "fprod1", 2, f_exclusive, 1));
 
 // Test FUTURE_SHARED
-resume( create(future_cons, 1024, 21, "fcons2", 1, f_shared) );
-resume( create(future_cons, 1024, 21, "fcons3", 1, f_shared) );
-resume( create(future_cons, 1024, 21, "fcons4", 1, f_shared) );
-resume( create(future_cons, 1024, 21, "fcons5", 1, f_shared) );
-resume( create(future_prod, 1024, 21, "fprod2", 2, f_shared, 2) );
+resume(create(future_cons, 1024, 21, "fcons2", 1, f_shared));
+resume(create(future_cons, 1024, 21, "fcons3", 1, f_shared));
+resume(create(future_cons, 1024, 21, "fcons4", 1, f_shared));
+resume(create(future_cons, 1024, 21, "fcons5", 1, f_shared));
+resume(create(future_prod, 1024, 21, "fprod2", 2, f_shared, 2));
 
-Test FUTURE_QUEUE
-resume( create(future_cons, 1024, 21, "fcons6", 1, f_queue) );
-resume( create(future_cons, 1024, 21, "fcons7", 1, f_queue) );
-resume( create(future_cons, 1024, 21, "fcons8", 1, f_queue) );
-resume( create(future_cons, 1024, 21, "fcons9", 1, f_queue) );
-resume( create(future_prod, 1024, 21, "fprod3", 2, f_queue, 3) );
-resume( create(future_prod, 1024, 21, "fprod4", 2, f_queue, 4) );
-resume( create(future_prod, 1024, 21, "fprod5", 2, f_queue, 5) );
-resume( create(future_prod, 1024, 21, "fprod6", 2, f_queue, 6) );
+//Test FUTURE_QUEUE
+resume(create(future_cons, 1024, 21, "fcons6", 1, f_queue));
+resume(create(future_cons, 1024, 21, "fcons7", 1, f_queue));
+resume(create(future_cons, 1024, 21, "fcons8", 1, f_queue));
+resume(create(future_cons, 1024, 21, "fcons9", 1, f_queue));
+resume(create(future_prod, 1024, 21, "fprod3", 2, f_queue, 3));
+resume(create(future_prod, 1024, 21, "fprod4", 2, f_queue, 4));
+resume(create(future_prod, 1024, 21, "fprod5", 2, f_queue, 5));
+resume( create(future_prod, 1024, 21, "fprod6", 2, f_queue, 6));
 
   return (0);
 }
