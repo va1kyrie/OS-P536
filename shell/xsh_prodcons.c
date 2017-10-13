1f9b93bd25a3a405ccf6d28e683145a2043b2fd0
Helen Dougherty <ladyl3na@gmail.com>
2017-10-13 09:43:35 -0400

    priorities

--- shell/xsh_prodcons.c
+++ shell/xsh_prodcons.c
@@ -29,25 +29,25 @@ f_shared    = future_alloc(FUTURE_SHARED);
 f_queue     = future_alloc(FUTURE_QUEUE);
 
 // Test FUTURE_EXCLUSIVE
-resume(create(future_cons, 1024, 20, "fcons1", 1, f_exclusive));
-resume(create(future_prod, 1024, 20, "fprod1", 2, f_exclusive, 1));
+//resume(create(future_cons, 1024, 20, "fcons1", 1, f_exclusive));
+//resume(create(future_prod, 1024, 20, "fprod1", 2, f_exclusive, 1));
 
 // Test FUTURE_SHARED
-resume(create(future_cons, 1024, 20, "fcons2", 1, f_shared));
-resume(create(future_cons, 1024, 20, "fcons3", 1, f_shared));
-resume(create(future_cons, 1024, 20, "fcons4", 1, f_shared));
-resume(create(future_cons, 1024, 20, "fcons5", 1, f_shared));
-resume(create(future_prod, 1024, 20, "fprod2", 2, f_shared, 2));
+resume(create(future_cons, 1024, 19, "fcons2", 1, f_shared));
+resume(create(future_cons, 1024, 19, "fcons3", 1, f_shared));
+resume(create(future_cons, 1024, 19, "fcons4", 1, f_shared));
+resume(create(future_cons, 1024, 19, "fcons5", 1, f_shared));
+resume(create(future_prod, 1024, 19, "fprod2", 2, f_shared, 2));
 
 //Test FUTURE_QUEUE
-resume(create(future_cons, 1024, 20, "fcons6", 1, f_queue));
-resume(create(future_cons, 1024, 20, "fcons7", 1, f_queue));
-resume(create(future_cons, 1024, 20, "fcons8", 1, f_queue));
-resume(create(future_cons, 1024, 20, "fcons9", 1, f_queue));
-resume(create(future_prod, 1024, 20, "fprod3", 2, f_queue, 3));
-resume(create(future_prod, 1024, 20, "fprod4", 2, f_queue, 4));
-resume(create(future_prod, 1024, 20, "fprod5", 2, f_queue, 5));
-resume( create(future_prod, 1024, 20, "fprod6", 2, f_queue, 6));
+resume(create(future_cons, 1024, 19, "fcons6", 1, f_queue));
+resume(create(future_cons, 1024, 19, "fcons7", 1, f_queue));
+resume(create(future_cons, 1024, 19, "fcons8", 1, f_queue));
+resume(create(future_cons, 1024, 19, "fcons9", 1, f_queue));
+resume(create(future_prod, 1024, 19, "fprod3", 2, f_queue, 3));
+resume(create(future_prod, 1024, 19, "fprod4", 2, f_queue, 4));
+resume(create(future_prod, 1024, 19, "fprod5", 2, f_queue, 5));
+resume( create(future_prod, 1024, 19, "fprod6", 2, f_queue, 6));
 
   return (0);
 }
