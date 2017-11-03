7ed3b780b05de052c22aaeb6396d7c97401a5a88
Helen Dougherty <ladyl3na@gmail.com>
2017-11-03 16:36:33 -0400

    try different approach to incremeting vars in write

--- system/fs.c
+++ system/fs.c
@@ -526,9 +526,10 @@ int fs_write(int fd, void *buf, int nbytes) {
       indb = indb - minb;
       fs_setmaskbit(blind);
       //keep track of data blocks in inode
-      oft[fd].in.blocks[i++] = blind;
+      oft[fd].in.blocks[i] = blind;
     }
     blind++;
+    i++;
   }
 
   status = fs_put_inode_by_num(0, oft[fd].in.id, &oft[fd].in);
